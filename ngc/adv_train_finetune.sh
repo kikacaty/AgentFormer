0331c27c60fae877ee95463d6ad5a1956d800203
Yulong Cao <yulongc@umich.edu>
2022-04-06 23:33:19 -0400

    update at

--- ngc/adv_train_finetune.sh
+++ ngc/adv_train_finetune.sh
@@ -11,9 +11,9 @@ do
         WS_ID=yulong-avg # replace with your workspace ID
 
 
-        NAME="sweep-mix-$BASENAME"
-        CMD="cd ~; \
-            apt-get update && apt-get install libgl1 -y \
+        NAME="finetune-$BASENAME"
+        CMD="cd /workspace/adv_pred/; git pull; \
+            apt-get update && apt-get install libgl1 -y; \
             pip install -r requirements.txt;\
             pip install wandb; wandb login 66e53af18f876c79bad2f274a73b1c8026ced2ef; \
             export WANDB_APIKEY=66e53af18f876c79bad2f274a73b1c8026ced2ef; "
