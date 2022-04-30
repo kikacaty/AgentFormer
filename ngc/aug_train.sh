9ac21238f82d23bffa735fc7fa281bae7f0312e8
Yulong Cao <yulongc@umich.edu>
2022-04-29 20:40:12 -0400

    update aug

---
+++ ngc/aug_train.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+for EPS in 0.05 0.1 0.2 0.3 0.5 1.0
+do
+    INS="dgx1v.16g.1.norm"
+
+    BASENAME="eps_${EPS} ml.model.adv_agentformer"
+
+    WS_ID=yulong-avg # replace with your workspace ID
+
+
+    NAME="$BASENAME.aug"
+    CMD="cd /workspace/adv_pred/; git pull; \
+        apt-get update && apt-get install libgl1 -y; \
+        pip install -r requirements.txt;\
+        pip install wandb; wandb login 66e53af18f876c79bad2f274a73b1c8026ced2ef; \
+        export WANDB_APIKEY=66e53af18f876c79bad2f274a73b1c8026ced2ef; "
+    CMD="${CMD} \ 
+        python aug_train.py --cfg adv_mini_nusc_5sample_pre \
+            --eps $EPS --benign --ngc"
+
+    echo "$CMD"
+
+    ngc batch run \
+    --instance "$INS" \
+    --name "$NAME" \
+    --image "nvidia/pytorch:21.02-py3" \
+    --workspace yulong-avg:/workspace \
+    --result /result \
+    --port 8888 \
+    --commandline "$CMD" 
+done
---
+++ scripts/aug_train.sh
@@ -0,0 +1,2 @@
+python aug_train.py --cfg adv_mini_nusc_5sample_pre --eps 0.1 --gpu 2 --aug noise \
+    --pretrained results/mini_nuscenes_5sample_agentformer_pre/models/model_0100.p --benign
